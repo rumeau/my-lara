c207112d91fb405b260b8bb5ceda0071497ebb4a
Jean Rumeau <rumeau@gmail.com>
2015-04-02 17:18:59 -0300

    Agrega view composer provider

---
+++ app/Providers/ViewComposerServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jean
+ * Date: 002 02 04 2015
+ * Time: 17:00
+ */
+
+namespace App\Providers;
+
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class ViewComposerServiceProvider
+ * @package App\Providers
+ */
+class ViewComposerServiceProvider extends ServiceProvider
+{
+    /**
+     *
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     *
+     */
+    public function register()
+    {
+
+    }
+}
